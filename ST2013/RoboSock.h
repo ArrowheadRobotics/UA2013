ef29505ad0ec1719eb6d4af9c71bef24651afb89
Alec Obradovich <aleco@aroltd.com>
2013-01-19 18:35:06 -0600

    Fixed linking errors due to rename

--- ST2013/RoboSock.h
+++ ST2013/RoboSock.h
@@ -51,7 +51,7 @@ int RoboSock::initConnection(char *ip, int port, bool blocking, bool conn) {
 	addr.sin_addr.s_addr = inet_addr(ip);
 	addr.sin_port = htons(port);
 	
-	fcntl(sock, (blocking)?O_BLOCK:O_NONBLOCK); // TODO fix this
+	//fcntl(sock, (blocking)?O_BLOCK:O_NONBLOCK); // TODO fix this
 	
 	return 0;
 }
