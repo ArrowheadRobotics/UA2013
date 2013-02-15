96961377dbb55a831c231fda82f8c537965d3020
Corin Rypkema <corinryp@gmail.com>
2013-02-15 16:56:01 -0600

    reincarnation of ugly.h
    Nothing needed to be said here

---
+++ CB13/ControllerIO.h
@@ -0,0 +1,51 @@
+// joyjstick paraphernalia
+// if you think i'm going to comment all of this, you're a madman
+
+
+#ifndef UGLY_H
+#define UGLY_H
+#define S1TRIG jstick1.GetTrigger()
+#define S1B2 jstick1.GetRawButton(2)
+#define S1B3 jstick1.GetRawButton(3)
+#define S1B4 jstick1.GetRawButton(4)
+#define S1B5 jstick1.GetRawButton(5)
+#define S1B6 jstick1.GetRawButton(6)
+#define S1B7 jstick1.GetRawButton(7)
+#define S1B8 jstick1.GetRawButton(8)
+#define S1B9 jstick1.GetRawButton(9)
+#define S1B10 jstick1.GetRawButton(10)
+#define S1B11 jstick1.GetRawButton(11)
+#define S1X jstick1.GetX()
+#define S1Y jstick1.GetY()
+#define S1Z jstick1.GetThrottle()
+
+#define S2TRIG jstick2.GetTrigger()
+#define S2B2 jstick2.GetRawButton(2)
+#define S2B3 jstick2.GetRawButton(3)
+#define S2B4 jstick2.GetRawButton(4)
+#define S2B5 jstick2.GetRawButton(5)
+#define S2B6 jstick2.GetRawButton(6)
+#define S2B7 jstick2.GetRawButton(7)
+#define S2B8 jstick2.GetRawButton(8)
+#define S2B9 jstick2.GetRawButton(9)
+#define S2B10 jstick2.GetRawButton(10)
+#define S2B11 jstick2.GetRawButton(11)
+#define S2X jstick2.GetX()
+#define S2Y jstick2.GetY()
+#define S2Z jstick2.GetThrottle()
+
+#define S3TRIG jstick3.GetTrigger()
+#define S3B2 jstick3.GetRawButton(2)
+#define S3B3 jstick3.GetRawButton(3)
+#define S3B4 jstick3.GetRawButton(4)
+#define S3B5 jstick3.GetRawButton(5)
+#define S3B6 jstick3.GetRawButton(6)
+#define S3B7 jstick3.GetRawButton(7)
+#define S3B8 jstick3.GetRawButton(8)
+#define S3B9 jstick3.GetRawButton(9)
+#define S3B10 jstick3.GetRawButton(10)
+#define S3B11 jstick3.GetRawButton(11)
+#define S3X jstick3.GetX()
+#define S3Y jstick3.GetY()
+#define S3Z jstick3.GetThrottle()
+#endif
