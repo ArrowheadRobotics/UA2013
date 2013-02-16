845b15bc0171bda9420b77dbe261f55f68fb7c29
NathanSchneider <nschnied95@gmail.com>
2013-02-16 12:10:34 -0600

    fixed everything

--- ST2013/Robot.h
+++ ST2013/Robot.h
@@ -23,26 +23,24 @@
 class Cyberhawk : public SimpleRobot {
 	NetworkTable *server;
 	
-	GenericController<Jaguar> 		drive; // todo replace with talon
+	GenericController<Jaguar> 		rive; // todo replace with talon
 	GenericController<Victor> 		vics;
-	GenericController<Solenoid>		noids;
-	GenericController<Relay> 		spikes;
-	GenericController<DigitalInput>	switches;
+	GenericController2Arg<Solenoid>		noids;
+	GenericController<DigitalInput>		switches;
 	GenericController<Joystick>		sticks;
+	GenericController2Arg<Encoder>		ncoders;
 	
-	GenericController2Arg<Encoder>	encoders;
-	
-	Compressor						compressor;
-	Counter							shootercounter;
+	Compressor				compressor;
+	Counter					shootercounter;
+	Relay					conveyor;
 	
 	// todo verify all of these enums
-	enum kDrive			{DR_RIGHT, DR_LEFT};
+	enum kDrive		{DR_RIGHT, DR_LEFT};
 	enum kVictors		{VI_SHOOTER, VI_ANGLER, VI_UNKNOWN1, VI_UNKNOWN2};
 	enum kSolenoids		{SO_CHUTE_LOW, SO_CHUTE_HI, SO_FIRING, SO_GATE, SO_FORK, SO_ARMS};
-	enum kSpikes		{SP_INTAKE, SP_COMPRESS};
 	enum kSwitches		{SPEED_SO, FORKLIFT_SO, ELEVATION_SO, COMP_PRESSURE };
 	enum KJoysticks		{RIGHT_JOY, LEFT_JOY, ACTIVE_JOY};
-	enum kEncoders		{EN_RAISER_A, EN_RAISER_B, EN_DRIVELEFT_A, EN_DRIVELEFT_B, EN_DRIVERIGHT_A, EN_DRIVERIGHT_B};
+	enum kEncoders		{EN_RAISER, EN_DRIVELEFT, EN_DRIVERIGHT};
 public:
 	Cyberhawk(void);
 	~Cyberhawk(void);
