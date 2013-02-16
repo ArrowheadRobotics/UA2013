6f625d564151e939d1e0da3821a77fe3bfe29be6
Corin Rypkema <corinryp@gmail.com>
2013-02-16 11:19:30 -0600

    Purge

---
+++ CB13/Commands/Dump.cpp
@@ -0,0 +1,30 @@
+#include "Dump.h"
+Dump::Dump() {
+	// Use requires() here to declare subsystem dependencies
+	requires(frisbeeHandler);
+}
+// Called just before this Command runs the first time
+void Dump::Initialize() {
+	SetTimeout(5.0f);
+	Robot::frisbeeHandler->dumpUp();
+	Robot::frisbeeHandler->conveyorDown();
+}
+// Called repeatedly when this Command is scheduled to run
+void Dump::Execute() {
+	
+}
+// Make this return true when this Command no longer needs to run execute()
+bool Dump::IsFinished() {
+	return IsTimedOut();
+}
+// Called once after isFinished returns true
+void Dump::End() {
+	Robot::frisbeeHandler->dumpDown();
+	Robot::frisbeeHandler->conveyorUp();
+}
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void Dump::Interrupted() {
+	Robot::frisbeeHandler->dumpDown();
+	Robot::frisbeeHandler->conveyorUp();
+}
