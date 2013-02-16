88e91b9f332a0d9da4ef257e8d34c3e9e9ece73b
Corin Rypkema <corinryp@gmail.com>
2013-02-16 10:25:35 -0600

    added functions to chute

--- CB13/Subsystems/Chute.cpp
+++ CB13/Subsystems/Chute.cpp
@@ -13,10 +13,22 @@ void Chute::InitDefaultCommand() {
 
 }
 
-void Chute::lowExtend(){}
-void Chute::lowRetract(){}
-void Chute::highExtend(){}
-void Chute::highRetract(){}
+void Chute::lowExtend(){
+	Robot::chute->chutelowextend->Set(true);
+	Robot::chute->chutelowretract->Set(false);
+}
+void Chute::lowRetract(){
+	Robot::chute->chutelowextend->Set(false);
+	Robot::chute->chutelowretract->Set(true);
+}
+void Chute::highExtend(){
+	Robot::chute->chutehighextend->Set(true);
+	Robot::chute->chutehighretract->Set(false);
+}
+void Chute::highRetract(){
+	Robot::chute->chutehighextend->Set(false);
+	Robot::chute->chutehighretract->Set(true);
+}
 
 void allOn(){
 	Robot::chute->chutehighextend->Set(true);
